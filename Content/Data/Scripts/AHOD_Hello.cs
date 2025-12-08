6f5b1c94707a09f8b5d450f39c9b550b99ab5d31
Tatu Holkko <tatu.holkko@gmail.com>
2025-12-09 01:14:38 +0200

    Add initial grid fetching

--- Content/Data/Scripts/AHOD_Hello.cs
+++ Content/Data/Scripts/AHOD_Hello.cs
@@ -1,28 +1,47 @@
 using Sandbox.ModAPI;
 using VRage.Utils;
 using VRage.Game.Components;
+using System.Collections.Generic;
+using VRage.ModAPI;
+using VRage.Game.ModAPI;
 
 namespace AHOD
 {
-    // This tells SE to load your component and call UpdateBeforeSimulation
     [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation)]
     public class AHOD_Hello : MySessionComponentBase
     {
-        public override void LoadData()
-        {
-            MyLog.Default.WriteLine("AHOD: Hello World – LoadData()");
-        }
 
         public override void UpdateBeforeSimulation()
         {
-            // Only run once
-            if (!_printed)
+            if (!_init)
             {
-                MyLog.Default.WriteLine("AHOD: Hello World – UpdateBeforeSimulation()");
-                _printed = true;
+                MyLog.Default.WriteLine("AHOD: Initializing...");
+                InitGrids();
+                MyLog.Default.WriteLine("AHOD: Initialization done.");
+                _init = true;
             }
         }
 
-        private bool _printed = false;
+        private void InitGrids()
+        {
+            HashSet<IMyEntity> entities = new HashSet<IMyEntity>();
+            MyAPIGateway.Entities.GetEntities(entities, e => true);
+            MyAPIGateway.Utilities.ShowNotification($"AHOD: found {entities.Count} entities.", 20000, "White");
+            MyLog.Default.WriteLine($"AHOD: found {entities.Count} entities.");
+            foreach (IMyEntity ent in entities)
+            {
+                var grid = ent as IMyCubeGrid;
+                if (grid == null)
+                {
+                    continue;
+                }
+                string name = grid.DisplayName;
+                MyAPIGateway.Utilities.ShowNotification($"AHOD: found grid: {name}", 2000, "White");
+                MyLog.Default.WriteLine($"AHOD: Found grid: {name}");
+            }
+        }
+
+        private HashSet<IMyCubeGrid> _grids;
+        private bool _init = false;
     }
 }
