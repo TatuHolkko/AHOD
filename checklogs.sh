64999f7d8b83bc5e7055730842c5f91aedfbe8bb
Tatu Holkko <tatu.holkko@gmail.com>
2025-12-29 16:46:39 +0200

    Add debug log saving to file

--- checklogs.sh
+++ checklogs.sh
@@ -1,7 +1,9 @@
 pushd "C:\Users\Tatu\AppData\Roaming\SpaceEngineers"
+outp="E:\gitrepos\AHOD\lastlog.log"
 f=$(printf "%s\n" SpaceEngineers_* | sort | tail -n1)
 if [ -f "$f" ]; then
-    tail -n500 "$f" | grep --color=auto "AHOD"
+    tail -n500 "$f" | grep --color=auto "AHOD" > "$outp"
+	cat "$outp"
 else
     echo "No SpaceEngineers logs found"
 fi
